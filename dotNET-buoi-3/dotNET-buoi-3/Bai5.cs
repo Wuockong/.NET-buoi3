8db106d9c5e78c73ded36ab5a88009c6d0b22fe8
Bui Quoc Cong <congbui123123@gmail.com>
2023-08-30 15:40:28 +0700

    Cap nhat buoi 3

---
+++ dotNET-buoi-3/dotNET-buoi-3/Bai5.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNET_buoi_3
+{
+    class HangThucPham
+    {
+        private string maHang;
+        private string tenHang = "xxx";
+        private double donGia;
+        private DateTime ngaySanXuat = DateTime.Now;
+        private DateTime ngayHetHan = DateTime.Now;
+
+        public string MaHang
+        {
+            get { return maHang; }
+            private set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                {
+                    maHang = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Mã hàng không được để trống");
+                }
+            }
+        }
+
+        public string TenHang
+        {
+            get { return tenHang; }
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                {
+                    tenHang = value;
+                }
+            }
+        }
+
+        public double DonGia
+        {
+            get { return donGia; }
+            set
+            {
+                if (value >= 0)
+                {
+                    donGia = value;
+                }
+            }
+        }
+
+        public DateTime NgaySanXuat
+        {
+            get { return ngaySanXuat; }
+            set
+            {
+                if (value <= DateTime.Now)
+                {
+                    ngaySanXuat = value;
+                }
+            }
+        }
+
+        public DateTime NgayHetHan
+        {
+            get { return ngayHetHan; }
+            set
+            {
+                if (value > ngaySanXuat)
+                {
+                    ngayHetHan = value;
+                }
+            }
+        }
+
+        public HangThucPham(string maHang, string tenHang, double donGia, DateTime ngaySanXuat, DateTime ngayHetHan)
+        {
+            MaHang = maHang;
+            TenHang = tenHang;
+            DonGia = donGia;
+            NgaySanXuat = ngaySanXuat;
+            NgayHetHan = ngayHetHan;
+        }
+
+        public HangThucPham(string maHang)
+            : this(maHang, "xxx", 0, DateTime.Now, DateTime.Now)
+        {
+        }
+
+        public bool KiemTraDaHetHan()
+        {
+            return DateTime.Now > ngayHetHan;
+        }
+
+        public override string ToString()
+        {
+            return "Ma hang: " + MaHang + ", Ten hang: " + TenHang + ", Đon gia: " + DonGia.ToString("N0")
+                + ", Ngay san xuat: " + NgaySanXuat.ToString("dd/MM/yyyy") + ", Ngay het han: "
+                + NgayHetHan.ToString("dd/MM/yyyy") + ", Đa het han: " + KiemTraDaHetHan();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                HangThucPham hang1 = new HangThucPham("MH001", "Banh quy", 15000, new DateTime(2023, 8, 1), new DateTime(2024, 8, 1));
+                HangThucPham hang2 = new HangThucPham("MH002");
+                HangThucPham hang3 = new HangThucPham("MH003", "Nuoc ngot", 10000, new DateTime(2023, 7, 1), new DateTime(2023, 8, 1));
+
+                Console.WriteLine(hang1);
+                Console.WriteLine(hang2);
+                Console.WriteLine(hang3);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Lỗi: " + ex.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ dotNET-buoi-3/dotNET-buoi-3/Bai6.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace dotNET_buoi_3
+{
+    class Account
+    {
+        private long accNum;
+        private string name;
+        private double balance;
+        private double RATE = 0.035;
+
+        public Account()
+        {
+            accNum = 999999;
+            name = "chua xac đinh";
+            balance = 50000;
+        }
+
+        public Account(long accNum, string name, double balance)
+        {
+            if (accNum > 0)
+                this.accNum = accNum;
+            else
+                this.accNum = 999999;
+
+            if (!string.IsNullOrWhiteSpace(name))
+                this.name = name;
+            else
+                this.name = "chua xac đinh";
+
+            if (balance >= 50000)
+                this.balance = balance;
+            else
+                this.balance = 50000;
+        }
+
+        public Account(long accNum, string name)
+            : this(accNum, name, 50000)
+        {
+        }
+
+        public bool Deposit(double amount)
+        {
+            if (amount > 0)
+            {
+                balance += amount;
+                return true;
+            }
+            return false;
+        }
+
+        public bool Withdraw(double amount, double fee)
+        {
+            if (amount > 0 && amount + fee <= balance)
+            {
+                balance -= (amount + fee);
+                return true;
+            }
+            return false;
+        }
+
+        public void AddInterest()
+        {
+            balance += balance * RATE;
+        }
+
+        public bool Transfer(Account acc2, double amount)
+        {
+            if (amount > 0 && Withdraw(amount, 0))
+            {
+                acc2.Deposit(amount);
+                return true;
+            }
+            return false;
+        }
+
+        public override string ToString()
+        {
+            CultureInfo local = new CultureInfo("vi-VN");
+            NumberFormatInfo formatter = (NumberFormatInfo)local.NumberFormat.Clone();
+            formatter.CurrencySymbol = "đ";
+
+            return "So tai khoan: " + accNum + ", Ten tai khoan: " + name + ", So du: " + balance.ToString("C", formatter);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account acc1 = new Account(72354, "Ted Murphy", 102.56);
+            Account acc2 = new Account(69713, "Jane Smith", 40.00);
+            Account acc3 = new Account(93757, "Edward Demsey", 759.32);
+
+            acc1.Deposit(25.85);
+            acc2.Deposit(500.00);
+
+            acc2.Withdraw(430.75, 1.50);
+
+            acc3.AddInterest();
+
+            Console.WriteLine("Thong tin acc1: " + acc1);
+            Console.WriteLine("Thong tin acc2: " + acc2);
+            Console.WriteLine("Thong tin acc3: " + acc3);
+
+            acc2.Transfer(acc1, 100.00);
+
+            Console.WriteLine("Thong tin acc1 sau chuyen tien: " + acc1);
+            Console.WriteLine("Thong tin acc2 sau chuyen tien: " + acc2);
+
+            Console.ReadKey();
+        }
+    }
+}
